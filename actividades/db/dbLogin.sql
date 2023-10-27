9d58b118a9a65cf87362f23bee0dc69082627393
Anndrydar <andrycedeno128@gmail.com>
2023-10-27 02:15:54 -0500

    Agregacion de la db login

---
+++ actividades/db/dbLogin.sql
@@ -0,0 +1,13 @@
+--creacion tabla de registro para las empresas 
+create table empresa(
+idEmpresa serial primary key,
+ruc varchar(20) unique,
+email text unique,
+telefono varchar(10),
+direccion text,
+nombre_empresa text,
+contacto text,
+fecha_ingreso timestamp default current_timestamp,
+ciudad text,
+password text
+);
